e1a1216dcf31d30a63b28dba67d3ca7f5dd15aa2
bruno signori <signoribruno@gmail.com>
2022-09-09 21:34:05 -0400

    added routes folder

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
 const fileUpload = require('express-fileupload');
 const port = 3000;
 
@@ -11,37 +10,7 @@ app.use(express.static('documents'));
 app.use(cors());
 app.use(fileUpload());
 
-app.get('/testing', (req,res) => {
-    let token = req.get('x-api-key');
-    let file = req.body.name;
-    if(fs.existsSync(`${__dirname}/documents/${file}`) && token){
-        const stream = fs.createReadStream(`${__dirname}/documents/${file}`);
-        stream.pipe(res);
-        stream.on('error',(err) => res.send('Invalid Request'));
-    }else{
-        return res.send('Invalid Request');
-    }
-});
-
-app.post('/test', async (req, res) => {
-    
-    let token = req.get('x-api-key');
-    let size = req.get('Content-Length');
-    if(!token || size > 2000000){
-        return res.send('Invalid Request');
-    }
-    try {
-
-        let file = req.files.upload;
-        uploadPath = __dirname + '/documents/' + file.name;
-        let request = await file.mv(uploadPath);
-        return res.json({'Response':'File Uploaded'});
-
-    } catch (err){
-        console.log(err);
-        return res.send('Invalid Request');
-    }  
-});
+require('./routes/routes.js')(app);
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
---
+++ routes/routes.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+module.exports = function(app){
+
+    app.get('/testing', (req,res) => {
+        let token = req.get('x-api-key');
+        let file = req.body.name;
+        if(fs.existsSync(`${__dirname}../../documents/${file}`) && token){
+            const stream = fs.createReadStream(`${__dirname}../../documents/${file}`);
+            stream.pipe(res);
+            stream.on('error',(err) => res.send('Invalid Request'));
+        }else{
+            return res.send('Invalid Request');
+        }
+    });   
+    app.post('/test', async (req, res) => {
+        
+        let token = req.get('x-api-key');
+        let size = req.get('Content-Length');
+        if(!token || size > 2000000){
+            return res.send('Invalid Request');
+        }
+        try {
+    
+            let file = req.files.upload;
+            uploadPath = __dirname + '../../documents/' + file.name;
+            let request = await file.mv(uploadPath);
+            return res.json({'Response':'File Uploaded'});
+    
+        } catch (err){
+            console.log(err);
+            return res.send('Invalid Request');
+        }  
+    });
+}
