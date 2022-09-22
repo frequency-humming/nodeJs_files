4758e0f88e2bcf3a2b21038c0639efa5e1a78527
bruno signori <signoribruno@gmail.com>
2022-09-22 19:19:34 -0400

    adding cors domain

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const cors = require('cors');
 const port = 3000;
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('documents'));
-app.use(cors());
 app.use(fileUpload());
+app.use(cors({
+    origin: 'https://www.*.my.salesforce.com'
+  }));
 
 require('./routes/routes.js')(app);
 
